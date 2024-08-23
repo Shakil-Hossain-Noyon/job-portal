5b0cab5a222327ca8db47102d7423862e58891b8
Shakil-Hossain-Noyon <shakil7584@gmail.com>
2024-08-23 15:16:50 +0600

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # This should already be set
+
+# Add this line
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
